71112bccfdac931febf7a7b41bf582c2c56c99fd
hant0508 <hant0508@gmail.com>
2016-09-10 20:00:20 +0300

    Add new words

---
+++ edit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ "$1" == "add" ]]
+then
+	s=`echo "$2" | tr ' ' '\n'`
+	echo "$s" >> dic.txt
+	sort -u dic.txt > dic.back
+	cp dic.back dic.txt
+elif [[ "$1" == "rm" ]]
+then
+	sed -i "/\<$2\>/d" dic.txt
+else
+	echo "add \"word\" or rm \"word\""
+fi
