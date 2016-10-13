a2a6cbc8a0a259eadd7277fef787755fd4ecf028
hant0508 <hant0508@gmail.com>
2016-10-13 18:49:48 +0300

    Add images preview

--- startup-settings/preview.js
+++ startup-settings/preview.js
@@ -2,6 +2,10 @@ var modal = document.getElementById('myModal');
 
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
+
+var imgs = document.getElementsByClassName("preview");
+console.log(imgs.length);
+
 function preview (img)
 {
     modal.style.display = "block";
