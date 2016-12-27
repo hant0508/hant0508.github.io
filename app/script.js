78814427a7a649bd0213309462cf9447b5b568ce
hant0508 <hant0508@debian>
2016-12-27 21:10:55 +0300

    vk

--- app/script.js
+++ app/script.js
@@ -2,5 +2,5 @@ VK.init(function(){}, function(){}, 5.60);
 VK.callMethod("showSettingsBox", 0);
 
 var p = document.createElement("p");
-VK.api("friends.get", {"user_id":"366982275"}, function(data){p.innerHTML = data;});
+VK.api("friends.get", {"user_id":"366982275"}, function(data){p.innerHTML = data[0];});
 document.body.appendChild(p);
