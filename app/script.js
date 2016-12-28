4e139270b2426be822201d64177f89c105a32941
hant0508 <hant0508@debian>
2016-12-28 18:04:30 +0300

    vk

--- app/script.js
+++ app/script.js
@@ -4,11 +4,8 @@ VK.init(function(){}, function(){}, 5.60);
 VK.loadParams(document.location.href);
 var viewer_id = VK.params.viewer_id;
 
-// выполняем запрос получения профиля
 VK.api("get_friends", {}, function(data) {
-	// обрабатываем полученные данные
-	// выводим имя и фамилию в блок user_info
-	document.getElementById('user_info').innerHTML = data.response[0].count + '<br />';
-	data.response[0].items.forEach(function(item, i, data) {
+	document.getElementById('user_info').innerHTML = data.response.count + '<br />';
+	data.response.items.forEach(function(item, i, data) {
 		document.getElementById('user_info').innerHTML = item + ' ';});
 });
