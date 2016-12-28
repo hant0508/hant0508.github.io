5a378cf12080e416e592f61f9a3d25eff233be87
hant0508 <hant0508@debian>
2016-12-28 18:13:37 +0300

    vk

--- app/script.js
+++ app/script.js
@@ -4,8 +4,8 @@ VK.init(function(){}, function(){}, 5.60);
 VK.loadParams(document.location.href);
 var viewer_id = VK.params.viewer_id;
 
-VK.api("get_friends", {}, function(data) {
-	document.getElementById('user_info').innerHTML = data.count + '<br />';
-	data.items.forEach(function(item, i, data) {
+VK.api("get.friends", {}, function(data) {
+	document.getElementById('user_info').innerHTML = data.response.count + '<br />';
+	data.response.items.forEach(function(item, i, data) {
 		document.getElementById('user_info').innerHTML = item + ' ';});
 });
