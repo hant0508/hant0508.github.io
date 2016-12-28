b93154c3c3863c03e182e5021b35f664730f875c
hant0508 <hant0508@debian>
2016-12-28 17:28:55 +0300

    vk

--- app/script.js
+++ app/script.js
@@ -1,6 +1,28 @@
 VK.init(function(){}, function(){}, 5.60);
 VK.callMethod("showSettingsBox", 0);
 
-var p = document.createElement("p");
-VK.api("friends.get", {"user_id":"366982275"}, function(data){p.innerHTML = data[0];});
-document.body.appendChild(p);
+// узнаём flashVars, переданные приложению GET запросом. Сохраняем их в переменную flashVars
+var parts=document.location.search.substr(1).split("&");
+var flashVars={}, curr;
+for (i=0; i<parts.length; i++)
+{
+	curr = parts[i].split('=');
+	// записываем в массив flashVars значения. Например: flashVars['viewer_id'] = 1;
+	flashVars[curr[0]] = curr[1];
+}
+
+// получаем viewer_id из полученных переменных
+var viewer_id = flashVars['viewer_id'];
+
+// выполняем запрос получения профиля
+VK.api("getProfiles", {uids:viewer_id,fields:"photo_big"}, function(data) {
+	// обрабатываем полученные данные
+	// выводим имя и фамилию в блок user_info
+	document.getElementById('user_info').innerHTML = data.response[0].first_name + ' ' + data.response[0].last_name + '<br />';
+	// создаем img, для отображения аватарки
+	var image=document.createElement('img');
+	// из полученных данных берем ссылку на фото
+	image.src=data.response[0].photo_big;
+	// добавляем img в блок user_info
+	user_info.appendChild(image);
+});
