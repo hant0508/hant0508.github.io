87a7dea7c70fdb2534b25112fb8a0389823446f2
hant0508 <hant0508@debian>
2017-01-14 18:59:31 +0300

    add some flood

--- seconds/seconds.js
+++ seconds/seconds.js
@@ -1,5 +1,5 @@
 var comment;
-var time;
+var time = -1;
 var url = document.getElementById("url");
 var res = document.getElementById("result");
 
@@ -9,25 +9,45 @@ function seconds(data) {
 	var postTime = $(parsed).find(".story__date").attr("title");
 	time = commentTime - postTime;
 	console.log(time);
-	out();
 }
 
-function out() {
+function setRes(str) {
+	res.innerHTML = str;
+}
+
+function out(s) {
+	if (time == -1) {
+		if (s == "...") s = "";
+		else s += ".";
+		window.setTimeout(function(){setRes("Считаем секунды"+s);out(s);}, 500);
+		return;
+	}
 	var sec = " секунд";
 	if (time % 100 < 10 || time % 100 > 20) {
 		if (time % 10 == 1) sec += "a";
 		else if (time % 10 < 5) sec += "ы";
 	}
-	res.innerHTML = time + sec;
+	setRes(time + sec);
 	url.focus();
 	url.select();
+}
+
+function flood() {
+	res.innerHTML = "Ищем комментарий";
+	window.setTimeout(function(){setRes("Ищем комментарий.");}, 500);
+	window.setTimeout(function(){setRes("Ищем комментарий..");}, 1000);
+	window.setTimeout(function(){setRes("Ищем комментарий...");}, 1500);
+	window.setTimeout(function(){setRes("Звоним L4rever");}, 2000);
+	window.setTimeout(function(){setRes("Звоним L4rever.");}, 2500);
+	window.setTimeout(function(){setRes("Звоним L4rever..");}, 3000);
+	window.setTimeout(function(){setRes("Звоним L4rever..."); out("");}, 3500);
 }
 
 document.forms.url.onsubmit = function() {
 	var str = url.value;
 	var post = str.slice(0, str.indexOf('#'));
 	comment = '#'+str.slice(str.indexOf('#')+1);
-	res.innerHTML = "Считаем секунды, ожидайте..."
 	$.get(post, seconds, "html");
+	flood();
 	return false;
 }
