3875e1977c137ce82c225e0d9d37b61342d51f5a
hant0508 <hant0508@gmail.com>
2017-01-15 23:35:32 +0300

    css and description

--- seconds/seconds.js
+++ seconds/seconds.js
@@ -15,10 +15,13 @@ function setRes(str) {
 function fail(code) {
 	switch(code) {
 		case 1:
-			setRes("Мы перерыли все комменты за последние 8 лет, наняли частного детектива и даже изучили архивы Пентагона, но так и не смогли найти нужный комментарий. <br /> Вы уверены, что он существует и не спрятан под кнопкой \"показать ещё\"? Тогда напишите об этом на <a href=\"mailto:hant0508@gmail.com?subject=GitHub issue | /seconds\">hant0508@gmail.com</a>.");
+			setRes("Мы перерыли все комменты за последние 8 лет, наняли частного детектива и даже изучили архивы Пентагона, но так и не смогли найти нужный комментарий. Вы уверены, что он существует и не спрятан под кнопкой \"показать ещё\"? Тогда напишите об этом на <a href=\"mailto:hant0508@gmail.com?subject=GitHub issue | /seconds\">hant0508@gmail.com</a>.");
 			break;
 		case 2:
 			setRes("С этим комментом что-то не так. <br /> Ссылка должна иметь вид <i>http://pikabu.ru/story/story_title#comment_12345678</i>");
+			break;
+		case 3:
+			break;
 	}
 
 	url.focus();
@@ -81,7 +84,12 @@ document.forms.url.onsubmit = function() {
 	var post = str.slice(0, str.indexOf('#'));
 	comment = '#'+str.slice(str.indexOf('#')+1);
 
-	if (!comment.match(/#comment_\d/) || !str.match(/https?:\/\/(www\.)?pikabu\.ru\/story/)) {
+	if (url.value.match(/^\s*$/)) {
+		fail(3);
+		return false;
+	}
+
+	else if (!comment.match(/#comment_\d/) || !str.match(/https?:\/\/(www\.)?pikabu\.ru\/story/)) {
 		fail(2);
 		return false;
 	}
